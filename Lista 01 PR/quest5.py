a85708b89d0b318bb39340141789ed2245083f86
LucasCiril <164411248+LucasCiril@users.noreply.github.com>
2024-03-26 17:57:35 -0300

    quest5.py

--- Lista 01 PR/quest5.py
+++ Lista 01 PR/quest5.py
@@ -8,26 +8,26 @@ minutos = float(input('Digite o número de minutos em que o carro ficou estacion
 hora_exced = (minutos - 240) / 60
 
 if minutos <= 60:
-    print('O valor a ser pago é de: 8,00 ')
+    print('O valor a ser pago é de: 8,00 R$')
 elif minutos <= 120:
-    print('O valor a ser pago é de: 16,00 ')
+    print('O valor a ser pago é de: 16,00 R$')
 elif minutos <= 180:
-    print('O valor a ser pago é de: 21,00 ')
+    print('O valor a ser pago é de: 21,00 R$')
 elif minutos <= 240:
-    print('O valor a ser pago é de: 26,00 ')
+    print('O valor a ser pago é de: 26,00 R$ ')
 elif minutos <= 300:
-    print('O valor a ser pago é de: 29,00 ')
+    print('O valor a ser pago é de: 29,00 R$ ')
 elif minutos <= 360:
-    print('O valor a ser pago é de: 32,00 ')
+    print('O valor a ser pago é de: 32,00 R$')
 elif minutos <= 420:
-    print('O valor a ser pago é de: 35,00 ')
+    print('O valor a ser pago é de: 35,00 R$')
 elif minutos <= 480:
-    print('O valor a ser pago é de: 38,00 ')
+    print('O valor a ser pago é de: 38,00 R$')
 elif minutos <= 540:
-    print('O valor a ser pago é de: 41,00 ')
+    print('O valor a ser pago é de: 41,00 R$')
 elif minutos <= 600:
-    print('O valor a ser pago é de: 44,00 ')
+    print('O valor a ser pago é de: 44,00 R$')
 elif minutos <= 660:
-    print('O valor a ser pago é de: 47,00 ')
+    print('O valor a ser pago é de: 47,00 R$')
 elif minutos >= 720:
-    print('O valor a ser pago é de: 30,00 ')
+    print('O valor a ser pago é de: 30,00 R$')
